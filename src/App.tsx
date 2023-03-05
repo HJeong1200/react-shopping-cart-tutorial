16fa8b66f7ce11c998146a6a6cc1a9464852a8d7
HJeong1200 <hjeong1200@gmail.com>
2023-03-06 01:40:03 +0900

    feat: get products from fakestoreapi and add CartItemType

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,29 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { Wrapper } from "./App.styles";
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> => {
+  return await (await fetch("https://fakestoreapi.com/products")).json();
+};
+
 function App() {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
+
+  console.log(data);
+
   return <div className="App">Hello!</div>;
 }
 
