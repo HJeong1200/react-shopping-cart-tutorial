fbae6cbfa818d60297595eeba13e18aceae93949
HJeong1200 <hjeong1200@gmail.com>
2023-03-07 23:33:48 +0900

    feat: add cart drawer and getTotalItems function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Grid, LinearProgress } from "@material-ui/core";
+import { Badge, Drawer, Grid, LinearProgress } from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
 import { useState } from "react";
 import { useQuery } from "react-query";
-import { Wrapper } from "./App.styles";
+import { StyledButton, Wrapper } from "./App.styles";
 import Item from "./components/Item";
 import { CartItemType } from "./types/CartItemType";
 
@@ -10,19 +11,30 @@ const getProducts = async (): Promise<CartItemType[]> => {
 };
 
 function App() {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
   const { data, isLoading, error } = useQuery<CartItemType[]>(
     "products",
     getProducts
   );
 
-  console.log(data);
-
   const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const getTotalItems = (item: CartItemType[]) =>
+    item.reduce((acc: number, item) => acc + item.amount, 0);
 
   if (isLoading) return <LinearProgress />;
   if (error) return <div>Something went wrong...</div>;
   return (
     <Wrapper>
+      <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
+        Cart
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(!cartOpen)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color="error">
+          <AddShoppingCart />
+        </Badge>
+      </StyledButton>
       <Grid container spacing={3}>
         {data?.map((item) => (
           <Grid item key={item.id} xs={12} sm={4}>
